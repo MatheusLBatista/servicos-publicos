f036feeb197fa58ee60a58162b9394b3d98c71f4
Matheus Batista <matheusifro2020@gmail.com>
2025-05-03 16:03:55 -0400

    criando model do usuário

--- src/models/demandas.js
+++ src/models/demandas.js
@@ -5,6 +5,8 @@ class Demanda {
     constructor() {
         const demandaSchema = new mongoose.Schema(
             {
+                id_demanda: { type: String, require: [true, "O ID da demanda é obrigatório!"]},
+                tipo: { type: String, require: [true, "O tipo da demanda é obrigatório!"]}
             }
         )
     }
--- src/models/usuarios.js
+++ src/models/usuarios.js
@@ -1,10 +1,44 @@
 import mongoose, { mongo } from "mongoose";
 import mongoosePaginate from "mongoose-paginate-v2";
 
+const estadosBrasil = [
+    "AC", "AL", "AP", "AM", "BA", "CE", "DF", "ES", "GO",
+    "MA", "MT", "MS", "MG", "PA", "PB", "PR", "PE", "PI",
+    "RJ", "RN", "RS", "RO", "RR", "SC", "SP", "SE", "TO"
+  ];
+
 class Usuario {
     constructor() {
         const usuarioSchema = new mongoose.Schema(
             {
+                id_usuario: { type: String, required: [true, "O ID do usuário é obrigatório!"]},
+                cpf: { type: String, required: [true, "O CPF do usuário é obrigatório!"] },
+                email: { type: String, required: [true, "O email do usuário é obrigatório!"] },
+                celular: { type: String, required: [true, "O celular do usuário é obrigatório!"] },
+                cnh: { type: String, required: [false, "A CNH do usuário não é obrigatória!"] },
+                data_nomeacao: { type: Date, required: [false, "A data de nomeação não é obrigatório!"] },
+                cargo: { type: String, required: [false, "O cargo do usuário não é obrigatório!"] },
+                formacao: { type: String, required: [false, "A formação do usuário não é obrigatório!"] },
+                nivel_acesso: {
+                    type: {
+                        municipe: { type: Boolean, required: true, default: true },
+                        operador: { type: Boolean, required: true, default: false },
+                        administrador: { type: Boolean, required: true, default: false }
+                    }, required: [ true, "O nível de acesso do usuário é obrigatório!"]
+                },
+                nome: { type: String, required: [true, "O nome do usuário é obrigatório!"] },
+                nome_social: { type: String, required: [false, "O nome social do usuário não é obrigatório!"] },
+                portaria_nomeacao: { type: String, required: [false, "A portaria de nomeação do usuário não é obrigatória!"] },
+                senha: { type: String, required: [true, "A senha do usuário é obrigatória!"] },
+                endereco: {
+                    logradouro: { type: String, required: [true, "O logradouro é obrigatório!"]},
+                    cep: { type: String, required: [true, "O CEP é obrigatório!"]},
+                    bairro: { type: String, required: [true, "O bairro é obrigatório!"]},
+                    numero: { type: String, required: [true, "O número é obrigatório!"]},
+                    complemento: { type: String, required: [true, "O complemento é obrigatório!"]},
+                    cidade: { type: String, required: [true, "O cidade é obrigatório!"]},
+                    estado: { type: String, enum: estadosBrasil, required: [true, "O estado é obrigatório!"]}
+                }
             }
         )
     }
