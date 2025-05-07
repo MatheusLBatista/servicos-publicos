669fd99cf60cd3ed99439f126a8f9257946411a4
matheus.lucas.batista <matheus20157813@gmail.com>
2025-05-07 19:23:30 -0400

    criando função main no seed.js

--- src/seeds/seeds.js
+++ src/seeds/seeds.js
@@ -1,4 +1,22 @@
 import "dotenv/config";
-
+import seedDemanda from "./seed_demanda";
+import seedUsuario from "./seed_usuario";
  
+async function main() {
+    try {
+      await seedDemanda();
+      await seedUsuario();
 
+      console.log(">>> SEED FINALIZADO COM SUCESSO! <<<");
+    } catch (err) {
+      console.error("Erro ao executar SEED:", err);
+
+    } finally {
+      mongoose.connection.close();
+      process.exit(0);
+    }
+  }
+  
+  // Executa tudo
+  main();
+  
