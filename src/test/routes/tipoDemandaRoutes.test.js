7483a231d07c2458b5ae44b8cc23af2b732ad4ff
Giullia Beatriz <giulliachiarotti@gmail.com>
2025-06-17 21:11:37 -0400

    implementando testes em tipoDemanda

---
+++ src/test/routes/tipoDemandaRoutes.test.js
@@ -0,0 +1,103 @@
+import express from "express";
+import tipoDemandaRoutes from '../../routes/tipoDemandaRoutes.js';
+import DbConnect from '../../config/dbConnect.js';
+import request from 'supertest';
+import errorHandler from '../../utils/helpers/errorHandler.js';
+
+let app; 
+
+beforeAll(async () => {
+  app = express(); 
+  await DbConnect.conectar(); 
+  app.use(express.json());
+  app.use(tipoDemandaRoutes);
+  app.use(errorHandler)
+});
+
+describe('Rotas de tipoDemanda', () => {
+  it('GET - Deve retornar uma lista dos tiposDemandas cadastradas', async () => {
+    const res = await request(app).get("/tipoDemanda");
+    //console.log(res.body);
+    expect(res.status).toBe(200);
+    expect(res.body.message).toBe("Requisição bem-sucedida");
+  });
+  it('GET - Deve retornar um tipoDemanda pelo ID', async () => {
+    const res = await request(app).get("/tipoDemanda/6832ad0c109564baed4cda14");
+    //console.log(res.body);
+    expect(res.status).toBe(200);
+    expect(res.body.message).toBe("Requisição bem-sucedida");
+    expect(res.body.data._id).toBe("6832ad0c109564baed4cda14");
+  });
+
+  it('GET - Deve retornar erro de recurso não encontrado em tipoDemanda pelo ID não existente', async () => {
+    const res = await request(app).get("/tipoDemanda/6832ad0c109564baed4cda11");
+    //console.log(res.body);
+    expect(res.status).toBe(404);
+    expect(res.body.message).toBe("Recurso não encontrado em TipoDemanda.");
+  });
+
+   it('POST - Deve cadastrar um novo tipoDemanda com sucesso', async () => {
+    const novoTipoDemanda = {
+      titulo: "Iluminação teste",
+      descricao: "exemplo",
+      subdescricao: "exemplo",
+      icone: "http://marson.org/fd432daf-ad5e-46fc-86ff-45e2ab126497.jpg",
+      link_imagem: "http://marson.org/fd432daf-ad5e-46fc-86ff-45e2ab126497.jpg",
+      tipo: "Iluminação",
+    };
+    const res = await request(app).post("/tipoDemanda").send(novoTipoDemanda);
+    //console.log(res.body);
+    expect(res.status).toBe(201);
+    expect(res.body).toHaveProperty("message");
+    expect(res.body).toHaveProperty("data");
+    expect(res.body.data).toHaveProperty("titulo", novoTipoDemanda.titulo);
+  });
+
+  it('POST - Deve retornar erro ao cadastrar um novo tipoDemanda com titulo vazio.', async () => {
+    const novoTipoDemanda = {
+      titulo: "",
+      descricao: "exemplo",
+      subdescricao: "exemplo",
+      icone: "http://marson.org/fd432daf-ad5e-46fc-86ff-45e2ab126497.jpg",
+      link_imagem: "http://marson.org/fd432daf-ad5e-46fc-86ff-45e2ab126497.jpg",
+      tipo: "Iluminação",
+    };
+    const res = await request(app).post("/tipoDemanda").send(novoTipoDemanda);
+    //console.log(res.body);
+    expect(res.status).toBe(400);
+    expect(res.body).toHaveProperty("message", "Erro de validação. 1 campo(s) inválido(s).");
+  });
+
+  it('PATCH - Deve atualizar parcialmente um tipoDemanda', async () => {
+    const atualizacao = {
+      titulo: "titulo atualizado",
+    };
+    const res = await request(app).patch(`/tipoDemanda/6832ad0c109564baed4cda14`).send(atualizacao);
+    expect(res.status).toBe(200);
+    expect(res.body.data).toHaveProperty("titulo", atualizacao.titulo);
+  });
+
+  it('PATCH - Deve retornar erro ao tentar atualizar um tipoDemanda inexistente', async () => {
+    const res = await request(app).patch(`/tipoDemanda/666666666666666666666666`).send({
+      titulo: "Teste inválido"
+    });
+    expect(res.status).toBe(404);
+    expect(res.body.message).toBe("Recurso não encontrado em TipoDemanda.");
+  });
+/*
+  it('DELETE - Deve deletar um tipoDemanda com sucesso', async () => {
+    const res = await request(app).delete("/tipoDemanda/68353b31ed620b3827f3f06c");
+    //console.log(res.body);
+    expect(res.status).toBe(200);
+    expect(res.body.message).toBe("TipoDemanda excluída com sucesso.");
+    expect(res.body.data._id).toBe("68353b31ed620b3827f3f06c");
+  });
+*/
+   it('DELETE - Deve retornar erro ao tentar deletar uma secretaria com id inválido', async () => {
+    const res = await request(app).delete("/tipoDemanda/68353b31ed620b3827f3f06c");
+    //console.log(res.body);
+    expect(res.status).toBe(404);
+    expect(res.body.message).toBe("Recurso não encontrado em TipoDemanda.");
+  });
+
+});
