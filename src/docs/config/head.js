0c26c25bb6a228150d28e5041eb11ea3e2786cfb
Giullia Beatriz <giulliachiarotti@gmail.com>
2025-07-09 20:05:33 -0400

    implementando rota dele em tipoDemanda swagger

---
+++ src/docs/config/head.js
@@ -0,0 +1,58 @@
+//import authPaths from "../paths/auth.js";
+import tipoDemandaPaths from "../paths/tipoDemanda.js";
+import tipoDemandaSchemas from "../schemas/tipoDemandaSchema.js";
+
+// Função para definir as URLs do servidor dependendo do ambiente
+const getServersInCorrectOrder = () => {
+    const devUrl = { url: process.env.SWAGGER_DEV_URL || `http://localhost:${PORT}` };
+    const prodUrl1 = { url: process.env.SWAGGER_PROD_URL || "https://api-servicos.exemplo.com" };
+
+    if (process.env.NODE_ENV === "production") return [prodUrl1, devUrl];
+    else return [devUrl, prodUrl1];
+};
+
+// Função para obter as opções do Swagger
+const getSwaggerOptions = () => {
+    return {
+        swaggerDefinition: {
+            openapi: "3.0.0",
+            info: {
+                title: "API de Serviços Públicos",
+                version: "1.0.0",
+                description: "",
+                contact: {
+                    name: "Serviços Públicos",
+                    email: "servicosPublicos@ifro.edu.br",
+                },
+            },
+            servers: getServersInCorrectOrder(),
+            tags: [
+                {
+                    name: "tipoDemanda",
+                    description: "Rotas para o gerenciamento de tipoDemanda"
+                },
+            ],
+            paths: {
+                ...tipoDemandaPaths,
+            },
+            components: {
+                securitySchemes: {
+                    bearerAuth: {
+                        type: "http",
+                        scheme: "bearer",
+                        bearerFormat: "JWT"
+                    }
+                },
+                schemas: {
+                    ...tipoDemandaSchemas,
+                }
+            },
+            security: [{
+                bearerAuth: []
+            }]
+        },
+        apis: ["./src/routes/*.js"]
+    };
+};
+
+export default getSwaggerOptions;
--- src/docs/paths/tipoDemanda.js
+++ src/docs/paths/tipoDemanda.js
@@ -36,6 +36,49 @@ const tipoDemandaRoutes = {
                 500: commonResponses[500]()
             }
         },
+        post: {
+            tags: ["TipoDemanda"],
+            summary: "Cadastro de novos tipoDemanda",
+            description: `
+            + Caso de uso: Permitir que o administrador cadastre um novo tipo de demanda no sistema.
+            
+            + Função de Negócio:
+                - Permitir à front-end permitir cadastrar um tipoDemanda.
+                + Recebe como path parameter:
+                    - **titulo**: titulo do tipoDemanda.
+                    - **descricao**: descrição do tipoDemanda.
+                    - **link_imagem**: link da imagem do tipoDemanda.
+                    - **icone**: link do icone do tipoDemanda.
+                    - **subdescricao**: subdescrição do tipoDemanda.
+                    - **tipo**: tipo do tipoDemanda.
+
+            + Regras de Negócio:
+                - O corpo da requisição deve seguir o TipoDemandaSchema.
+                - Campos obrigatórios como nome, descricao, subdescricao e tipo devem ser enviados.
+                - Não deve permitir a criação de tipos de demanda com nomes duplicados.
+
+            + Resultado Esperado:
+                - HTTP 200 OK retornando a mensagem de tipo de demanda criado com sucesso e retorna os dados do registro recém-criado, incluindo seu ID.
+        `,
+            security: [{ bearerAuth: [] }],
+             requestBody: {
+                content: {
+                    "application/json": {
+                        schema: {
+                            $ref: "#/components/schemas/TipoDemandaPost"
+                        }
+                    }
+                }
+            },
+            responses: {
+                200: commonResponses[200]("#/components/schemas/tipoDemandaPost"),
+                400: commonResponses[400](),
+                401: commonResponses[401](),
+                404: commonResponses[404](),
+                498: commonResponses[498](),
+                500: commonResponses[500]()
+            }
+        },
     },
     "/tipoDemanda/{id}": {
         get: {
@@ -75,24 +118,116 @@ const tipoDemandaRoutes = {
                 500: commonResponses[500]()
             }
         },
-    },
-    "/tipoDemanda": {
-        post: {
+        patch: {
             tags: ["TipoDemanda"],
-            summary: "Obtém detalhes de um tipo Demanda",
+            summary: "Atualização de tipoDemanda (PATCH)",
             description: `
-            + Caso de uso: Consulta de detalhes de um tipoDemanda específico.
+            + Caso de uso: - Permitir que o administrador atualize parcialmente ou totalmente os dados de um tipo de demanda existente.
             
             + Função de Negócio:
-                - Permitir à front-end obter todas as informações de um tipoDemanda cadastrado.
+                - Permitir à front-end permitir atualizar um tipoDemanda.
                 + Recebe como path parameter:
                     - **id**: identificador do tipoDemanda (MongoDB ObjectId).
 
             + Regras de Negócio:
-                - Validação do formato do ID.
+                - O ID deve ser validado com o TipoDemandaIDSchema.
+                - Os dados enviados devem seguir o TipoDemandaUpdateSchema.
+                - Não deve permitir a criação de tipos de demanda com nomes duplicados.
 
             + Resultado Esperado:
-                - HTTP 200 OK com corpo conforme **tipoDemandaDetalhes**, contendo dados completos do tipoDemanda.
+                - HTTP 200 OK e os dados do tipo de demanda são atualizados com sucesso e o sistema retorna os novos dados com uma mensagem de confirmação.
+        `,
+            security: [{ bearerAuth: [] }],
+            parameters: [
+                {
+                    name: "id",
+                    in: "path",
+                    required: true,
+                    schema: { type: "string" }
+                }
+            ],
+             requestBody: {
+                content: {
+                    "application/json": {
+                        schema: {
+                            $ref: "#/components/schemas/TipoDemandaPutPatch"
+                        }
+                    }
+                }
+            },
+            responses: {
+                200: commonResponses[200]("#/components/schemas/TipoDemandaPutPatch"),
+                400: commonResponses[400](),
+                401: commonResponses[401](),
+                404: commonResponses[404](),
+                498: commonResponses[498](),
+                500: commonResponses[500]()
+            }
+        },
+        put: {
+            tags: ["TipoDemanda"],
+            summary: "Atualização de tipoDemanda (PUT)",
+            description: `
+            + Caso de uso: - Permitir que o administrador atualize totalmente os dados de um tipo de demanda existente.
+            
+            + Função de Negócio:
+                - Permitir à front-end permitir atualizar um tipoDemanda.
+                + Recebe como path parameter:
+                    - **id**: identificador do tipoDemanda (MongoDB ObjectId).
+
+            + Regras de Negócio:
+                - O ID deve ser validado com o TipoDemandaIDSchema.
+                - Os dados enviados devem seguir o TipoDemandaUpdateSchema.
+                - Não deve permitir a criação de tipos de demanda com nomes duplicados.
+
+            + Resultado Esperado:
+                - HTTP 200 OK e os dados do tipo de demanda são atualizados com sucesso e o sistema retorna os novos dados com uma mensagem de confirmação.
+        `,
+            security: [{ bearerAuth: [] }],
+            parameters: [
+                {
+                    name: "id",
+                    in: "path",
+                    required: true,
+                    schema: { type: "string" }
+                }
+            ],
+             requestBody: {
+                content: {
+                    "application/json": {
+                        schema: {
+                            $ref: "#/components/schemas/TipoDemandaPutPatch"
+                        }
+                    }
+                }
+            },
+            responses: {
+                200: commonResponses[200]("#/components/schemas/TipoDemandaPutPatch"),
+                400: commonResponses[400](),
+                401: commonResponses[401](),
+                404: commonResponses[404](),
+                498: commonResponses[498](),
+                500: commonResponses[500]()
+            }
+        },
+        delete: {
+            tags: ["TipoDemanda"],
+            summary: "Deleta um tipoDemanda",
+            description: `
+            + Caso de uso: - Permitir que o administrador exclua um tipo de demanda do sistema, caso ele não esteja vinculado a nenhuma demanda ativa.
+            
+            + Função de Negócio:
+                - Permitir à front-end permitir atualizar um tipoDemanda.
+                + Recebe como path parameter:
+                    - **id**: identificador do tipoDemanda (MongoDB ObjectId).
+
+            + Regras de Negócio:
+                - O ID deve ser validado com o TipoDemandaIDSchema.
+                - Caso o tipo de demanda esteja associado a demandas existentes, a exclusão deve ser impedida.
+                - A existência do tipo de demanda deve ser verificada antes de excluí-lo.
+
+            + Resultado Esperado:
+                - HTTP 200 OK e o tipo de demanda é removido com sucesso do banco de dados e o sistema retorna uma mensagem de sucesso e os dados do TipoDemanda excluído.
         `,
             security: [{ bearerAuth: [] }],
             parameters: [
@@ -106,7 +241,7 @@ const tipoDemandaRoutes = {
                 }
             ],
             responses: {
-                200: commonResponses[200]("#/components/schemas/tipoDemandaPost"),
+                200: commonResponses[200](),
                 400: commonResponses[400](),
                 401: commonResponses[401](),
                 404: commonResponses[404](),
